8be2c04384c17cfe6b1f5a67e890914fe13e7931
Valerie <valerieraem@gmail.com>
2021-10-11 23:25:59 -0500

    fixing something

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyA6wysJumCIVKXjoDlPlxC7OBHeugPbTZ8",
+    authDomain: "moodstream-a3a53.firebaseapp.com",
+    projectId: "moodstream-a3a53",
+    storageBucket: "moodstream-a3a53.appspot.com",
+    messagingSenderId: "866038229156",
+    appId: "1:866038229156:web:6e562487f846a1846a1e31",
+    measurementId: "G-5H38BLNMKE"
+    
+  }
 };
