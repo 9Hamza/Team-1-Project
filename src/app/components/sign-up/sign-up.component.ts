2ebf59e2d22120317e9f15a0fe77700bea3dae5c
Hamza <hamza.bahamdan@gmail.com>
2021-10-20 16:59:46 -0500

    Add Authentication and Login Page. Modify Signup Page and Nav Bar. Add App Routing
    Added firebase authentication to be able to sign in and sign out using firebase database. Added a log in page, and modified the sign up page. Added seamless app routing between pages using Angular instead of using HTML hyperlinks.

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+    selector: 'app-sign-up',
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+    isProgressVisible: boolean;
+    signupForm: FormGroup = new FormGroup({});
+    firebaseErrorMessage: string;
 
-  ngOnInit(): void {
-  }
+    constructor(private authService: AuthService, private router: Router, private afAuth: AngularFireAuth) {
+        this.isProgressVisible = false;
+        this.firebaseErrorMessage = '';
+    }
 
+    ngOnInit(): void {
+        if (this.authService.userLoggedIn) {                       // if the user's logged in, navigate them to the dashboard (NOTE: don't use afAuth.currentUser -- it's never null)
+            this.router.navigate(['/dashboard']);
+        }
+
+        this.signupForm = new FormGroup({
+            'displayName': new FormControl('', Validators.required),
+            'email': new FormControl('', [Validators.required, Validators.email]),
+            'password': new FormControl('', Validators.required)
+        });
+    }
+
+    signup() {
+        if (this.signupForm.invalid)                            // if there's an error in the form, don't submit it
+            return;
+
+        this.isProgressVisible = true;
+        this.authService.signupUser(this.signupForm.value).then((result) => {
+            if (result == null)                                 // null is success, false means there was an error
+                this.router.navigate(['/dashboard']);
+            else if (result.isValid == false)
+                this.firebaseErrorMessage = result.message;
+
+            this.isProgressVisible = false;                     // no matter what, when the auth service returns, we hide the progress indicator
+        }).catch(() => {
+            this.isProgressVisible = false;
+        });
+    }
 }
