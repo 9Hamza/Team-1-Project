c415f6d65360c884a3bb0e544ddf0e2abaccf0ac
DyllanThomas <dmt2647@utulsa.edu>
2021-12-05 16:19:41 -0600

    Update api-calls.service.ts

--- src/app/services/api-calls.service.ts
+++ src/app/services/api-calls.service.ts
@@ -16,4 +16,12 @@ export class ApiCallsService {
   movieDetails(id:any) {
     return this.http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=ea13feb29808cba44ae41a961107c167');
   }
+
+  watchFeature(id:any) {
+    return this.http.get('https://api.themoviedb.org/3/movie/' + id + '/watch/providers?api_key=ea13feb29808cba44ae41a961107c167');
+  }
+
+  getCast(id:any){
+    return this.http.get('https://api.themoviedb.org/3/movie/' + id + '/credits?api_key=ea13feb29808cba44ae41a961107c167');
+  }
 }
